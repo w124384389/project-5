5184b747f9d4dc6da42147c24502ef4dd46df519
Chen <wangc16@hanover.edu>
2015-10-18 16:21:04 -0400

    modify index.html and script.js

--- js/script.js
+++ js/script.js
@@ -1,125 +1,179 @@
+
+
 /*global Google */
 
 //Model: object that store all the location information
 var Model = {
-    currentPlace: ko.observable(null),
     initialPlaces:[
         {
-    		name:"Broad Ripple Park",
-    		lat:39.86987,
-    		lng:-86.13311,
-    		address:"1550 Broad Ripple Ave, Indianapolis, IN 46220"
-    	},
+            name:"Broad Ripple Park",
+            lat:39.86987,
+            lng:-86.13311,
+            address:"1550 Broad Ripple Ave, Indianapolis, IN 46220"
+        },
 
-    	{
-    		name:"Humane Society of Indianapolis",
-    		lat:39.89966,
-    		lng:-86.21649,
-    		address:"7929 N Michigan Rd, Indianapolis, IN 46268"
-    	},
+        {
+            name:"Humane Society of Indianapolis",
+            lat:39.89966,
+            lng:-86.21649,
+            address:"7929 N Michigan Rd, Indianapolis, IN 46268"
+        },
 
-    	{
-    		name:"Smock Bark Park",
-    		lat:39.63974,
-    		lng:-86.09653,
-    		address:"4200 E County Line Rd, Indianapolis, IN 46237"
-    	},
+        {
+            name:"Smock Bark Park",
+            lat:39.63974,
+            lng:-86.09653,
+            address:"4200 E County Line Rd, Indianapolis, IN 46237"
+        },
 
-    	{
-    		name:"Club Canine Doggie",
-    		lat:39.97141,
-    		lng:-86.12785,
-    		address:"622 S Rangeline Rd, Carmel, IN 46032"
-    	},
+        {
+            name:"Club Canine Doggie",
+            lat:39.97141,
+            lng:-86.12785,
+            address:"622 S Rangeline Rd, Carmel, IN 46032"
+        },
 
-    	{
-    		name:"Pierson Bark Park",
-    		lat:39.97904,
-    		lng:-85.95624,
-    		address:"11787 E 131st St, Fishers, IN 46038"
-    	}
-    ]
+        {
+            name:"Pierson Bark Park",
+            lat:39.97904,
+            lng:-85.95624,
+            address:"11787 E 131st St, Fishers, IN 46038"
+        }
+    ],
+    mapOptions: {
+        center: {lat: 39.86987, lng: -86.13311},
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+        mapTypeControlOptions: {
+            position: google.maps.ControlPosition.TOP_CENTER
+        }
+    }
 };
 
 
 //initialize the map
-var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 10,
-    center: {lat:39.86987, lng:-86.13311}
-});
+// var map;
+// function initMap(){
+     var map = new google.maps.Map(document.getElementById('map'),Model.mapOptions);
+// };
+
+
 
 //initialize the markers and info window and animation
+
 var Place = function(data){
     var self = this;
     self.name = ko.observable(data.name);
     self.address = ko.observable(data.address);
     self.lat = ko.observable(data.lat);
     self.lng = ko.observable(data.lng);
-    //self.showList = ko.observable(true);
     
-    //pins locations on the map   
-    var marker = new google.maps.Marker({
-        position: {lat:data.lat, lng:data.lng},
-        title: data.name,
-        map: map,
-        draggable: false,
-        animation: google.maps.Animation.DROP
-    });
-
-
-    //show location information when clicked
-    var infoWindow = new google.maps.InfoWindow({
-      content: data.name 
-    });
-
-    //markers bounce and shows inforWindow when clicked once, then stop bouncing and close
-    //info windows after another click
-    function toggleBounce() {
-        if (marker.getAnimation() !== null) {
-            marker.setAnimation(null);
-            infoWindow.close(map, marker);
-        } else {
-            marker.setAnimation(google.maps.Animation.BOUNCE);
-            infoWindow.open(map, marker);
-        }
-    };
-    marker.addListener('click', toggleBounce);
-
 };
 
 
 //octopus
 var ViewModel = function(){
     var self = this;
-    self.searchList = ko.observableArray(Model.initialPlaces);
+    //self.searchList = ko.observableArray(Model.initialPlaces);
     self.query = ko.observable('');
-    self.placeList = ko.observableArray([]);
-    //search box
-    self.search = ko.computed(function(){
-        return ko.utils.arrayFilter(self.searchList(), function(place){
-            var s = place.name.toLowerCase().indexOf(self.query().toLowerCase());
-            return  s>= 0;
-            s.toggleBounce();
-        });
-    });
-    //initialize marker
+    self.placeList = ko.observableArray();
+
+    //push all locations into ko observableArray
     Model.initialPlaces.forEach(function(place){
         self.placeList.push(new Place(place));
     });
 
-    // //click
-    // this.currentPlace = ko.observable(this. placeList()[0]);
-    // //this.currentPlace.addListener('click', toggleBounce);
-    // this.setPlace = function(clickedPlace){
-    //     //self.currentPlace(clickedPlace);
-    //     //marker.addListener('click', toggleBounce);
-    //     console.log("hi");
-    // };
+    //the current place is the first one in the list by default
+    self.currentPlace = ko.observable(self.placeList()[0]);
+
+    self.infoWindow = ko.observableArray();
+    self.marker = ko.observableArray();
+
+    //search box, filter places
+    self.search = ko.computed(function(){
+        if (!self.query()){
+            return self.placeList();
+        } else {
+        return ko.utils.arrayFilter(self.placeList(), function(place){
+            return place.name().toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
+        });
+        }
+    });
+
+    var currentInfoWindow;
+    //filter the markers and show the related marker info window
+    this.markerSearch = function(){
+        for (var i = 0; i < self.marker().length; i++){
+            //hide all markers at first
+            self.marker()[i].setVisible(false);
+            if (self.query().toLowerCase() === '' || self.marker()[i].name.toLowerCase().indexOf(self.query().toLowerCase()) > -1){
+                //show marker if match the search
+                self.marker()[i].setVisible(true);
+            }
+            typeof currentInfoWindow !== 'undefined' && currentInfoWindow.close();
+            currentInfoWindow = self.infoWindow()[i];
+        }
+    };
+
+    self.query.subscribe(self.markerSearch);
+
+    //enable markers with infowindow
+    ko.computed(function(){
+        for (var i = 0; i < Model.initialPlaces.length; i++){
+            //markers
+            self.marker.push(new google.maps.Marker({
+                position: {lat:Model.initialPlaces[i].lat, lng:Model.initialPlaces[i].lng},
+                title: Model.initialPlaces[i].name,
+                map: map,
+                draggable: false,
+                animation: google.maps.Animation.DROP
+        
+            }));
+            self.infoWindow.push(new google.maps.InfoWindow({
+                content: Model.initialPlaces[i].name 
+            }));
+            //click to markers
+            (function(i){
+                google.maps.event.addListener(self.marker()[i], 'click', function() {
+                    mapEvents(i);
+                    // set currentPlace to the clicked marker
+                    self.setPlace(self.placeList()[i]);
+                });
+            })(i);
+        }
+    }, this);
+
+    function mapEvents(i){
+        // close prev infowindow
+        typeof currentInfoWindow !== 'undefined' && currentInfoWindow.close();
+        // show infowindow
+        self.infoWindow()[i].open(map, self.marker()[i]);
+        // center map to current marker
+        //map.panTo(self.markers()[i].getPosition());
+        // animate marker
+        self.marker()[i].setAnimation(google.maps.Animation.BOUNCE);
+        // animate marker only once
+        setTimeout(function(){ self.marker()[i].setAnimation(null); }, 750);
+
+        currentInfoWindow = self.infoWindow()[i];
+    }
+
+    // set current Sight
+    self.setPlace = function(place) {
+        self.currentPlace(place);
+        // list items click events
+        for (var i in self.marker()) {
+            if (self.marker()[i].name == place.name()) {
+                // add some click marker click logic here
+                mapEvents(i);
+            }
+        }
+    };
+
 };
 
-//helper function: check the viewport
-function windowSize() {
-    
-}
+// //helper function: check the viewport
+// function windowSize() {
+// }
 
-ko.applyBindings(new ViewModel);
+ko.applyBindings(new ViewModel());
